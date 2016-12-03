14a0cee2da08acd8b6fffee948b52d23c04ebd6e
8tomat8 <8tomat8@gmail.com>
2016-12-03 22:20:04 +0200

    Added jobs counter

--- workers/handler.go
+++ workers/handler.go
@@ -1,9 +1,15 @@
 package workers
 
-import "github.com/8tomat8/GoRepost/task"
+import (
+	"github.com/8tomat8/GoRepost/counter"
+	"github.com/8tomat8/GoRepost/task"
+)
 
 // Handler of all incoming tasks
 func Handler(t *task.Task) {
+	c := counter.GetCounter()
+	c.JobStarted()
+	defer c.JobFinished()
 	for _, dest := range t.Destinations {
 		switch social := dest.Social; social {
 		case "vk":
